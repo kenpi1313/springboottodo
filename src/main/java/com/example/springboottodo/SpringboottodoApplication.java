4007aedef77811e364d0b55dac611d3ca8fdfcd9
kawaharakenta <kawaharakenta@team-lab.com>
2020-09-03 14:38:38 +0900

    initial commit

---
+++ src/main/java/com/example/springboottodo/SpringboottodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottodoApplication.class, args);
+	}
+
+}
